96758cb8bb1c664b25f9515b457dec074e6a2aec
flyingpenguin0 <91243754+flyingpenguin0@users.noreply.github.com>
2022-01-03 12:05:00 +0900

    Update timeformatter.ts
    .

--- client/src/utilities/timeformatter.ts
+++ client/src/utilities/timeformatter.ts
@@ -1,6 +1,13 @@
+const toTwoDigits= (num:number) : string => {
+	if(num<10){
+		return `0${num}`;
+	} else return `${num}`;
+}
+
 export const timeFormatter = (num : number) : string => {
-	if(num>=3600 && num<=86400) return `${Math.floor(num/3600)}Hr ${Math.floor((num%3600)/60)}Min  ${num%60}Sec}`;
-	else if(num<3600 && num>=60) return `${Math.floor(num/60)}Min ${num%60}Sec`;
-	else if(num<60 && num>=0) return `${num}Sec`;
+	if(num>=3600 && num<=86400) return `${toTwoDigits(Math.floor(num/3600))} : ${toTwoDigits(Math.floor((num%3600)/60))} :  ${toTwoDigits(num%60)}}`;
+	else if(num<3600 && num>=60) return `00 : ${toTwoDigits(Math.floor(num/60))} : ${toTwoDigits(num%60)}`;
+	else if(num<60 && num>=0) return `00 : 00 : ${toTwoDigits(num)}`;
 	else return `0`;
 }
+
