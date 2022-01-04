791700f28523eacdd859ef12eaaa9598dfaa5e08
flyingpenguin0 <91243754+flyingpenguin0@users.noreply.github.com>
2022-01-04 15:45:13 +0900

    Update GamePage.tsx

--- client/src/components/GamePage.tsx
+++ client/src/components/GamePage.tsx
@@ -1,34 +1,82 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 //components
-import Navbar from "./Navbar";
 import GameBoard from "./Gameboard";
 import GameBoardContainer from "./Gameboard/container";
 import Console from "./Console";
 import SelectorContainer from "./NewGameSelector/selectorContainer";
 import styled from "styled-components";
-
+import { RootState } from "../modules";
+import { useSelector } from "react-redux";
+import ModalScreen from "./Modals/ModalScreen";
+import ModalGameEnd from "./Modals/ModalGameEnd";
+import ModalGameOver from "./Modals/ModalGameOver";
 
 const GamePage : React.FC = () => {
+    const Game = useSelector((state:RootState) => state.game);
+    const [toggled, setToggled] = useState([false, false]);
+    const onClickScreen = () : void => {
+        let newState = [false, false];
+        setToggled(newState);
+    }
+
+    useEffect(()=>{
+        // refresh state?
+        if(Game.gameEnd){
+            let newState = [false,true];
+            setToggled(newState);
+        }
+        if(Game.gameOver){
+            let newState =[true,false];
+            setToggled(newState);
+        }
+    },[Game]);
+
     return(
-        <div>
-            <Navbar/>
+        <Wrapper>
             <ConsoleWrapper>
                 <Console/>
                 <SelectorContainer/>
             </ConsoleWrapper>
             <GameBoardContainer/>
-        </div>
+            {
+            toggled[0]
+            ? (
+                <>
+                <ModalGameOver />
+                <ModalScreen toggle={onClickScreen}/>
+                </>
+            )
+            : (null)
+            } {
+            toggled[1]
+                ? (
+                <>
+                    <ModalGameEnd toggle={onClickScreen}/>
+                    <ModalScreen toggle={onClickScreen}/>
+                </>
+                )
+                : (null)
+            }
+        </Wrapper>
     )
 }
 
 export default GamePage;
+const Wrapper = styled.div`
+    width:100%;
+    height:100vh;
+    background-color:${props=>props.theme.background};
+`;
 
 const ConsoleWrapper = styled.div`
     width: 100%;
     height : fit-content;
     padding : 1rem;
-    margin : 1rem;
     display : flex;
-    justify-content:space-between;
+    justify-content:space-evenly;
     align-items:center;
+    
+    @media (max-width: 991.98px){
+        display:block;
+    }
 `;
