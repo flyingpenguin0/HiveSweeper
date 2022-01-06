5afb615377d125e64d761daddae8fba06a5628f9
flyingpenguin0 <91243754+flyingpenguin0@users.noreply.github.com>
2022-01-06 18:16:29 +0900

    Update NotFoundPage.tsx

--- client/src/components/NotFoundPage.tsx
+++ client/src/components/NotFoundPage.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import notfound from "../static/404.png";
+import styled from "styled-components";
 
-const NotFoundPage : React.FC = () => {
+const NotFoundPage = () => {
     return(
-        <div>Not Found</div>
+        <NotFound/>
     )
 }
+const NotFound = styled.div`
+    width:100%;
+    height:100vh;
+    background-image: url(${notfound});
+    background-size:cover;
+    background-repeact:no-repeat;
+`;
 
 export default NotFoundPage;
