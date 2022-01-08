553b166ab05690a85fb08922ca2a93dd59b76575
flyingpenguin0 <91243754+flyingpenguin0@users.noreply.github.com>
2022-01-09 02:23:06 +0900

    Deleted unused package imports
    .

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import axios from "axios";
 // redux
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./modules";
-import ReduxThunk from "redux-thunk";
 import logger from "redux-logger";
-//axios
-//axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:8000/" : "/";
-//axios.defaults.headers.post["Content-Type"] = "application/json";
+
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 console.log(store.getState());
 
